913d81656e82a99c75a128c0c411b8c678d19117
Max Diamond <diamax98@gmail.com>
2022-10-07 17:43:12 -0400

    Learn about creating DBs and tbls, and inserting data into tables

---
+++ create_dbs_and_tbls.sql
@@ -0,0 +1,52 @@
+-- source C:\Users\611391\Documents\Learning\MySQL\create_dbs_and_tbls.sql
+
+
+-- 
+-- DATABASES
+-- 
+
+-- Create DB
+-- CREATE DATABASE test_db;
+
+
+-- Dropping DB
+-- DROP DATABASE test_db;
+
+
+-- Using DBs
+-- CREATE DATABASE test_db;
+-- USE test_db;
+
+-- See which DB you're currently using
+-- SELECT DATABASE();
+-- DROP DATABASE test_db;
+
+
+-- 
+-- TABLES
+-- 
+
+-- Create table
+-- CREATE DATABASE cat_app;
+-- USE cat_app;
+-- CREATE TABLE cats
+--     (
+--         name    VARCHAR(100),
+--         age     INT
+--     );
+
+
+-- Delete table
+-- USE cat_app;
+-- DROP TABLE cats; 
+
+
+-- Challenge - Create my own table!
+CREATE DATABASE pastry_shop;
+USE pastry_shop;
+CREATE TABLE pastries
+    (
+        name        VARCHAR(30),
+        quantity    INT
+    );
+    
