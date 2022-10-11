0bc0cd1cee4f8811125e11c5a76394d981d44347
Max Diamond <diamax98@gmail.com>
2022-10-11 17:27:07 -0400

    Added file to track mist SQL fns, learned from leetcode

---
+++ misc_funcs.sql
@@ -0,0 +1,11 @@
+/*
+
+GROUP_CONCAT
+    - Returns row values in comma separated fashion
+
+col1    col2    ->      col1    col2
+X       'A'             X       'A','B'
+X       'B'             Y       'A'
+Y       'A'
+
+*/
