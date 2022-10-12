2ad6c2da1977fda6328afcbaa80a1645ab6cb51d
Max Diamond <diamax98@gmail.com>
2022-10-12 11:20:33 -0400

    Added "USING" and "HAVING"

--- misc_funcs.sql
+++ misc_funcs.sql
@@ -8,4 +8,17 @@ X       'A'             X       'A','B'
 X       'B'             Y       'A'
 Y       'A'
 
+
+USING
+    - Join 2 tables that share the same column name, and only use that 1 column
+
+col1    col2a     +     col1       col2b    ->      col1    col2a   col2b
+1       Aa              2          Bb               1       ...     ...
+2       Ba              3          Cb               ...
+4       Da              4          Db
+
+
+HAVING
+    - Used to subquery aggregated data from a GROUP BY
+
 */
